9e3f5ddb01b38148dae6eb3e382e3c973c5cb000
Rosamelanillo67 <marc.fradera@institutvidreres.cat>
2026-03-25 10:41:30 +0100

    Jugadors

--- app/src/main/java/com/marcfradera/shooterranking/ui/screens/JugadorsRankingScreen.kt
+++ app/src/main/java/com/marcfradera/shooterranking/ui/screens/JugadorsRankingScreen.kt
@@ -21,6 +21,14 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
